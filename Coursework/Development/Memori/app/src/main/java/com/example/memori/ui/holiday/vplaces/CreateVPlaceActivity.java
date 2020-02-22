3e1aa3df576c3b4f699dd584739a5ce5c404d6a4
Bhaven19 <38040347+Bhaven19@users.noreply.github.com>
2020-02-22 20:28:25 +0000

    Attempt to Implement Create VPlace

--- Coursework/Development/Memori/app/src/main/java/com/example/memori/ui/holiday/vplaces/CreateVPlaceActivity.java
+++ Coursework/Development/Memori/app/src/main/java/com/example/memori/ui/holiday/vplaces/CreateVPlaceActivity.java
@@ -1,16 +1,269 @@
 package com.example.memori.ui.holiday.vplaces;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.memori.R;
+import com.example.memori.components.HolidayDate;
 
-public class CreateVPlaceActivity extends AppCompatActivity {
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Calendar;
+
+public class CreateVPlaceActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText textViewVPlaceName, textViewVPlaceDate, textViewVPlaceLoc, textViewVPlaceTravellers, textViewVPlaceNotes;
+    private ImageView imageViewVPlaceImage;
+    private Button mAddImage, mSaveVPlace, btnDate;
+
+    private final Calendar c = Calendar.getInstance();
+    private int mYear, mMonth, mDay;
+    private Boolean validDate = false;
+    private HolidayDate date;
+
+    private int GALLERY = 1, CAMERA = 2;
+    private static final String IMAGE_DIRECTORY = "/memori/";
+    private Boolean pictureSaved = false;
+    private String mImagePath;
+    private Bitmap bmpHolidayImage;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_vplace);
+
+        textViewVPlaceName = findViewById(R.id.edit_Name);
+        textViewVPlaceLoc = findViewById(R.id.edit_StartLoc);
+        textViewVPlaceTravellers = findViewById(R.id.edit_EndLoc);
+        textViewVPlaceNotes = findViewById(R.id.edit_Notes);
+
+        textViewVPlaceDate = findViewById(R.id.edit_StartDate);
+        textViewVPlaceDate.setEnabled(false);
+
+        mAddImage = findViewById(R.id.btn_saveImage);
+        mAddImage.setOnClickListener(this);
+
+        mSaveVPlace = findViewById(R.id.btn_saveHoliday);
+        mSaveVPlace.setOnClickListener(this);
+
+        btnDate = findViewById(R.id.btn_startDate);
+        btnDate.setOnClickListener(this);
+
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_saveVPlace:
+                Intent saveHolidayIntent = new Intent();
+
+                if (TextUtils.isEmpty(textViewVPlaceName.getText()) || TextUtils.isEmpty(textViewVPlaceDate.getText()) ) {
+
+                    setResult(0, saveHolidayIntent);
+
+                    Toast.makeText(getApplicationContext(), "Fields are empty, nothing was saved", Toast.LENGTH_LONG).show();
+
+                } else {
+                    String vPlaceName = textViewVPlaceName.getText().toString();
+                    String vPlaceDestination = textViewVPlaceLoc.getText().toString();
+                    String vPlaceCompanions = textViewVPlaceTravellers.getText().toString();
+                    String vPlaceNotes = textViewVPlaceNotes.getText().toString();
+                    String vPlaceDate = "";
+
+                    if (date != null){
+                        vPlaceDate = date.toString();
+                    }
+
+                    String vPlaceImagePath = mImagePath;
+
+                    saveHolidayIntent.putExtra("vPlaceName", vPlaceName);
+                    saveHolidayIntent.putExtra("vPlaceDestination", vPlaceDestination);
+                    saveHolidayIntent.putExtra("vPlaceCompanions", vPlaceCompanions);
+                    saveHolidayIntent.putExtra("vPlaceNotes", vPlaceNotes);
+                    saveHolidayIntent.putExtra("vPlaceDate", vPlaceDate);
+                    saveHolidayIntent.putExtra("hImagePath", vPlaceImagePath);
+
+                    setResult(1, saveHolidayIntent);
+                }
+                finish();
+
+                break;
+            case R.id.btn_saveVPlaceImage:
+                if (pictureSaved == false) {
+                    AlertDialog.Builder pictureDialog = new AlertDialog.Builder(this);
+                    pictureDialog.setTitle("Select Action");
+                    String[] pictureDialogItems = {
+                            "Select Image from gallery",
+                            "Take new Image with camera"};
+                    pictureDialog.setItems(pictureDialogItems,
+                            (dialog, which) -> {
+                                switch (which) {
+                                    case 0:
+                                        choosePhotoFromGallary();
+                                        break;
+                                    case 1:
+                                        takePhotoFromCamera();
+                                        break;
+                                }
+                            });
+                    pictureDialog.show();
+
+                    pictureSaved = true;
+
+                } else {
+                    displayToast("You can only save 1 image, any new images will overwrite previous images");
+
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+
+                    }
+
+                    AlertDialog.Builder pictureDialog = new AlertDialog.Builder(this);
+                    pictureDialog.setTitle("Select Action");
+                    String[] pictureDialogItems = {
+                            "Select Image from gallery",
+                            "Take new Image with camera"};
+                    pictureDialog.setItems(pictureDialogItems,
+                            (dialog, which) -> {
+                                switch (which) {
+                                    case 0:
+                                        choosePhotoFromGallary();
+                                        break;
+                                    case 1:
+                                        takePhotoFromCamera();
+                                        break;
+                                }
+                            });
+                    pictureDialog.show();
+
+
+                }
+
+                //imageViewVPlaceImage.setImageBitmap(bmpHolidayImage);
+
+                break;
+            case R.id.btn_selectVPlaceDate:
+                // Get Current Date
+                mYear = c.get(Calendar.YEAR);
+                mMonth = c.get(Calendar.MONTH);
+                mDay = c.get(Calendar.DAY_OF_MONTH);
+                validDate = false;
+
+                DatePickerDialog startDatePickerDialog = new DatePickerDialog(this,
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                date = new HolidayDate(dayOfMonth, monthOfYear, year);
+
+                                textViewVPlaceDate.setText(date.toString());
+
+                            }
+                        }, mYear, mMonth, mDay);
+                startDatePickerDialog.show();
+                break;
+        }
+    }
+
+    public void choosePhotoFromGallary() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        startActivityForResult(galleryIntent, GALLERY);
+    }
+
+    private void takePhotoFromCamera() {
+        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, CAMERA);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == this.RESULT_CANCELED) {
+            return;
+        }
+        if (requestCode == GALLERY) {
+            if (data != null) {
+                Uri contentURI = data.getData();
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), contentURI);
+                    String path = saveImage(bitmap);
+
+                    bmpHolidayImage = bitmap;
+
+                    Toast.makeText(CreateVPlaceActivity.this, "Image Saved!", Toast.LENGTH_SHORT).show();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Toast.makeText(CreateVPlaceActivity.this, "Failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        } else if (requestCode == CAMERA) {
+            Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+            String path = saveImage(thumbnail);
+
+            bmpHolidayImage = thumbnail;
+
+            Toast.makeText(CreateVPlaceActivity.this, "Image Saved!", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+    public String saveImage(Bitmap myBitmap) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        myBitmap.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
+        File wallpaperDirectory = new File(Environment.getExternalStorageDirectory() + IMAGE_DIRECTORY);
+        // have the object build the directory structure, if needed.
+        if (!wallpaperDirectory.exists()) {
+            wallpaperDirectory.mkdirs();
+        }
+
+        try {
+            File f = new File(wallpaperDirectory, Calendar.getInstance()
+                    .getTimeInMillis() + ".jpg");
+            f.createNewFile();
+            FileOutputStream fo = new FileOutputStream(f);
+            fo.write(bytes.toByteArray());
+            MediaScannerConnection.scanFile(this,
+                    new String[]{f.getPath()},
+                    new String[]{"image/jpeg"}, null);
+            fo.close();
+            Log.d("ImageFind", "CreateHoliday: " + f.getAbsolutePath());
+            mImagePath = f.getAbsolutePath();
+
+            return f.getAbsolutePath();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        return "";
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+
+    }
+
 }
