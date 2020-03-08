ed17c1c7e796204c74744d65818fe1993e4cfd7e
Bhaven19 <38040347+Bhaven19@users.noreply.github.com>
2020-03-08 16:19:57 +0000

    Implemented Current Location in EditVPlace

--- Coursework/Development/Memori/app/src/main/java/com/example/memori/ui/holiday/vplaces/EditVPlace.java
+++ Coursework/Development/Memori/app/src/main/java/com/example/memori/ui/holiday/vplaces/EditVPlace.java
@@ -1,5 +1,6 @@
 package com.example.memori.ui.holiday.vplaces;
 
+import android.Manifest;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -15,8 +16,10 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
@@ -28,20 +31,38 @@ import com.example.memori.database.HolidayRepository;
 import com.example.memori.database.entities.Holiday;
 import com.example.memori.database.entities.Images;
 import com.example.memori.database.entities.VisitedPlace;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.Task;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.FetchPlaceRequest;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+import com.tbruyelle.rxpermissions2.RxPermissions;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
 
 public class EditVPlace extends AppCompatActivity implements View.OnClickListener{
 
     private HolidayRepository holidayRepository;
 
     private EditText textViewVPlaceName, textViewVPlaceDate, textViewVPlaceLoc, textViewVPlaceTravellers, textViewVPlaceNotes;
+    private TextView textViewVPlaceAddress;
     private Spinner spinnerChooseHoliday;
     private ImageView imageViewVPlaceImage;
     private Button mAddImage, mSaveVPlace, btnDate;
@@ -56,6 +77,7 @@ public class EditVPlace extends AppCompatActivity implements View.OnClickListene
     private Boolean pictureSaved = false;
     private String mImagePath;
     private Bitmap bmpHolidayImage;
+    private ImageButton mGetCurrentLoc;
 
     private ArrayList<String> allHolidays;
     private HashMap<String, Integer> adapterVisitedPlaces;
@@ -68,6 +90,11 @@ public class EditVPlace extends AppCompatActivity implements View.OnClickListene
     private Images vPlaceImage;
 
     private Boolean setSpinner = false;
+
+    private Boolean permissionsGranted = false;
+
+    private String placeID = "";
+    private Place chosenPlace;
 
 
     @Override
@@ -82,6 +109,7 @@ public class EditVPlace extends AppCompatActivity implements View.OnClickListene
 
         textViewVPlaceName = findViewById(R.id.edit_vPlaceName);
         textViewVPlaceDate = findViewById(R.id.edit_VPlaceDate);
+        textViewVPlaceAddress = findViewById(R.id.label_VPlaceAddress);
         textViewVPlaceTravellers = findViewById(R.id.edit_vPlaceCompanions);
         textViewVPlaceNotes = findViewById(R.id.edit_vPlaceNotes);
 
@@ -99,6 +127,11 @@ public class EditVPlace extends AppCompatActivity implements View.OnClickListene
         setValues();
 
         setupSpinner();
+
+        mGetCurrentLoc = findViewById(R.id.btn_getCurrentLocation);
+        mGetCurrentLoc.setOnClickListener(this);
+
+        setupAutoComplete();
     }
 
     @Override
@@ -116,7 +149,7 @@ public class EditVPlace extends AppCompatActivity implements View.OnClickListene
                 } else {
                     String vPlaceHoliday = spinnerChooseHoliday.getSelectedItem().toString();
                     mVisitedPlace.setName(textViewVPlaceName.getText().toString());
-                    mVisitedPlace.setLocation(textViewVPlaceLoc.getText().toString());
+                    mVisitedPlace.setLocation(placeID);
                     mVisitedPlace.setTravellers(textViewVPlaceTravellers.getText().toString());
                     mVisitedPlace.setNotes(textViewVPlaceNotes.getText().toString());
 
@@ -211,6 +244,67 @@ public class EditVPlace extends AppCompatActivity implements View.OnClickListene
                             }
                         }, mYear, mMonth, mDay);
                 startDatePickerDialog.show();
+                break;
+            case R.id.btn_getCurrentLocation:
+                FusedLocationProviderClient fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);;
+
+                if (!permissionsGranted) {
+                    RxPermissions rxPermissions = new RxPermissions(this);
+
+                    Log.d("TrackLocation", "getActivity: " + this);
+
+                    rxPermissions.request(Manifest.permission.ACCESS_FINE_LOCATION).subscribe(granted -> {
+                        if (granted) {
+                            Log.d("VPlaceTrackLocation", "Permissions Granted");
+                            permissionsGranted = true;
+                        } else {
+                            Log.d("VPlaceTrackLocation", "Permissions Denied");
+                            permissionsGranted = false;
+                            // At least one permission is denied
+                        }
+                    });
+                }
+
+                if (permissionsGranted){
+                    if (!Places.isInitialized()) {
+                        Places.initialize(getApplicationContext(), "AIzaSyDMPsU2SV31MnUAONzl0WEI2iEDkU31kZ0", Locale.UK);
+                    }
+
+                    // Use fields to define the data types to return.
+                    List<Place.Field> placeFields = (Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.ADDRESS, Place.Field.LAT_LNG));
+
+                    // Use the builder to create a FindCurrentPlaceRequest.
+                    FindCurrentPlaceRequest request = FindCurrentPlaceRequest.newInstance(placeFields);
+
+                    PlacesClient placesClient = Places.createClient(this);
+
+                    Task<FindCurrentPlaceResponse> placeResponse = placesClient.findCurrentPlace(request);
+                    placeResponse.addOnCompleteListener(task -> {
+                        if (task.isSuccessful()){
+                            FindCurrentPlaceResponse response = task.getResult();
+
+                            chosenPlace = response.getPlaceLikelihoods().get(0).getPlace();
+
+                            placeID = chosenPlace.getId();
+                            textViewVPlaceAddress.setText("Address: " + chosenPlace.getAddress());
+
+                            Log.d("VPlaceTrackLocation", "Most Likely Place Name: " + chosenPlace.getName());
+                            Log.d("VPlaceTrackLocation", "Most Likely Place ID: " + chosenPlace.getId());
+                            Log.d("VPlaceTrackLocation", "Most Likely Place getAddress: " + chosenPlace.getAddress());
+
+                        } else {
+                            Exception exception = task.getException();
+
+                            if (exception instanceof ApiException) {
+                                ApiException apiException = (ApiException) exception;
+
+                                Log.d("VPlaceTrackLocation", "Place not found: " + apiException.getStatusCode());
+
+                            }
+                        }
+                    });
+
+                }
                 break;
         }
     }
@@ -341,7 +435,6 @@ public class EditVPlace extends AppCompatActivity implements View.OnClickListene
 
         textViewVPlaceName.setText(isNull(mVisitedPlace.getName()));
         textViewVPlaceDate.setText(isNull(mVisitedPlace.getDate()));
-        textViewVPlaceLoc.setText(isNull(mVisitedPlace.getLocation()));
         textViewVPlaceTravellers.setText(isNull(mVisitedPlace.getTravellers()));
         textViewVPlaceNotes.setText(isNull(mVisitedPlace.getNotes()));
 
@@ -350,6 +443,11 @@ public class EditVPlace extends AppCompatActivity implements View.OnClickListene
             mImagePath = vPlaceImage.getPath();
             mImageDate = vPlaceImage.getDate();
             mImageTag = vPlaceImage.getTag();
+        }
+
+        placeID = mVisitedPlace.getLocation();
+        if (placeID != null){
+            setAddress();
         }
 
 
@@ -361,6 +459,68 @@ public class EditVPlace extends AppCompatActivity implements View.OnClickListene
         } else {
             return value;
         }
+    }
+
+    public void setAddress(){
+        if (!Places.isInitialized()) {
+            Places.initialize(getApplicationContext(), "AIzaSyDMPsU2SV31MnUAONzl0WEI2iEDkU31kZ0", Locale.UK);
+        }
+
+        Log.d("VPlaceLocation", "ViewVPlace: placeID: " + placeID);
+        // Define a Place ID.
+        String placeId = placeID;
+
+        // Specify the fields to return.s
+        List<Place.Field> placeFields = (Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.ADDRESS, Place.Field.LAT_LNG));
+
+        // Construct a request object, passing the place ID and fields array.
+        FetchPlaceRequest request = FetchPlaceRequest.newInstance(placeId, placeFields);
+
+        PlacesClient placesClient = Places.createClient(getApplicationContext());
+        placesClient.fetchPlace(request).addOnSuccessListener((response) -> {
+            chosenPlace = response.getPlace();
+            Log.d("VPlaceLocation", "ViewVPlace: Place found: " + chosenPlace.getName());
+
+            textViewVPlaceAddress.setText("Address: " + chosenPlace.getAddress());
+
+        }).addOnFailureListener(e ->
+                Log.d("VPlaceLocation", "ViewVPlace: Place Not Found"));
+    }
+
+    public void setupAutoComplete(){
+        // Initialize the AutocompleteSupportFragment.
+        if (!Places.isInitialized()) {
+            Places.initialize(getApplicationContext(), "AIzaSyDMPsU2SV31MnUAONzl0WEI2iEDkU31kZ0", Locale.UK);
+        }
+
+        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
+                getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+
+        // Specify the types of place data to return.
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.ADDRESS, Place.Field.LAT_LNG));
+
+        // Set up a PlaceSelectionListener to handle the response.
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+                Log.d("PlacesAutoComplete", "Place: " + place.getName() + ", " + place.getId());
+
+                placeID = place.getId();
+                textViewVPlaceAddress.setText("Address: " + place.getAddress());
+
+                Log.d("PlacesAutoComplete", "CreateVPlace: getAddress: " + place.getAddress());
+                //Log.d("PlacesAutoComplete", "CreateVPlace: Lat, Long: " + place.getLatLng().latitude + ", " + place.getLatLng().longitude);
+
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.d("PlacesAutoComplete", "An error occurred: " + status);
+            }
+        });
+
     }
 
 }
